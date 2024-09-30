ef2584d8b8a192b4c3c121ed8d0aa5f297816082
Andre Rodrigues <andrerodrigues@pinterest.com>
2024-09-30 17:44:33 -0300

    Add CDD to booklistDTO

--- internal/dtos/books.go
+++ internal/dtos/books.go
@@ -4,6 +4,7 @@ type BookListDto struct {
 	Id                int    `json:"id"`
 	Title             string `json:"title"`
 	RegisterNumber    int    `json:"registerNumber"`
+	CDD               string `json:"cdd"`
 	PublisherName     string `json:"publisherName" gorm:"references:publisher.name"`
 	AuthorsNames      string `json:"authorsNames"`
 	LendingId         int    `json:"lendingId,omitempty"`
